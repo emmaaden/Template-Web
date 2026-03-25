e094a8a0d7c882945ad93c9ee0b7dc728eaab3bf
Emmanuel Denis <emma26228@gmail.com>
2026-03-25 08:25:48 -0300

    Se agrego en el menu filtros por categoria

--- Public/js/menu.js
+++ Public/js/menu.js
@@ -1,5 +1,7 @@
+let productosGlobal = []
 $(document).ready(function () {
     cargarMenu()
+
 })
 
 async function cargarMenu() {
@@ -17,6 +19,10 @@ async function cargarMenu() {
     try {
         const res = await fetch("/api/productos?filtro=CATEGORIA&ascdesc=ture");
         const data = await res.json();
+
+        productosGlobal = data
+        renderCategorias(data)
+
         const container = $("#menuContainer");
         container.html("");
         let categorias = [];
@@ -52,4 +58,94 @@ async function cargarMenu() {
         console.error(error);
     }
     Swal.close();
+}
+
+function renderProductos(data) {
+
+    const container = $("#menuContainer")
+
+    container.html("")
+
+    data.forEach(item => {
+
+        container.append(`
+
+            <div class="col-md-4 mb-4">
+
+                <div class="card menu-card">
+
+                    <img src="${item.imagen}" class="card-img-top">
+
+                    <div class="card-body">
+
+                        <h5>${item.NOMBRE}</h5>
+
+                        <p>${item.DESCRIPCION}</p>
+
+                        <div class="price">$${item.PRECIO}</div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            `)
+
+    })
+
+}
+
+function filtrarCategoria(categoria, el) {
+
+    $(".categoria-pill").removeClass("categoria-activa")
+
+    $(el).addClass("categoria-activa")
+
+    if (categoria === "todos") {
+        cargarMenu()
+        return
+    }
+
+    const filtrados = productosGlobal.filter(p => p.CATEGORIAS.NOMBRE === categoria)
+
+    renderProductos(filtrados)
+
+}
+
+function renderCategorias(data) {
+
+    const categorias = [...new Set(data.map(p => p.CATEGORIAS.NOMBRE))]
+
+    const container = $("#categoriasContainer")
+
+    container.html("")
+
+    // botón "todos"
+    container.append(`
+        <div class="categoria-pill categoria-activa" onclick="filtrarCategoria('todos', this)">
+            Todos
+        </div>
+        `)
+
+    categorias.forEach(cat => {
+
+        container.append(`
+            <div class="categoria-pill" onclick="filtrarCategoria('${cat}', this)">
+                ${getEmoji(cat)} ${cat}
+            </div>
+        `)
+    })
+
+}
+
+function getEmoji(cat) {
+    cat = cat.toLowerCase()
+
+    if (cat.includes("hamburguesa")) return "🍔"
+    if (cat.includes("pizza")) return "🍕"
+    if (cat.includes("bebida")) return "🥤"
+    if (cat.includes("lomo")) return "🥪"
+
+    return "🍽"
 }
